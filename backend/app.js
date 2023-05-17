4db7f4e15985bb8558de63ef1fe51caef8e139f0
Olga Filchukova <makeeva.o.y@gmail.com>
2023-05-17 11:05:09 +0300

    front+back v11

--- backend/app.js
+++ backend/app.js
@@ -43,7 +43,8 @@ app.use((req, res, next) => {
   return next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+// mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
